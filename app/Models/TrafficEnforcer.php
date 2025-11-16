7f11caf8c6c614f6e09167d2a4e588c22d9ed710
PaulBertLandicho <paulbertlandicho.202200536@gmail.com>
2025-11-16 10:20:25 +0800

    Updated Commit

--- app/Models/TrafficEnforcer.php
+++ app/Models/TrafficEnforcer.php
@@ -6,25 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class TrafficEnforcer extends Model
 {
-    protected $table = 'traffic_enforcers';
-    protected $primaryKey = 'enforcer_id';
-    public $timestamps = false;
-    public $incrementing = false;
-    protected $keyType = 'string';
+        protected $table = 'traffic_enforcers';
+        protected $primaryKey = 'enforcer_id';
+        public $timestamps = false;
+        public $incrementing = false;
+        protected $keyType = 'string';
 
-    protected $fillable = [
-        'enforcer_id',
-        'enforcer_name',
-        'enforcer_email',
-        'enforcer_password',
-        'assigned_area',
-        'profile_image',
-        'contact_no',
-        'gender',
-        'registered_at',
-        'code',
-        'enforcer_signature',
-        'is_locked',
-        'role_id'
-    ];
+        protected $fillable = [
+                'enforcer_id',
+                'enforcer_name',
+                'enforcer_email',
+                'enforcer_password',
+                'assigned_area',
+                'profile_image',
+                'contact_no',
+                'gender',
+                'registered_at',
+                'code',
+                'enforcer_signature',
+                'is_locked',
+                'role_id'
+        ];
 }
